c41bb793322ba3a8843944474c28a7f391486119
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2022-12-27 13:40:15 +0700

    Sắp xong rồi nha mọi người

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 var flag = 1;
 var x = document.getElementById("main");
 function openNav() {
-  document.getElementById("mySidebar").style.width = "13rem";
-  document.getElementById("body").style.width = "86%";
-  document.getElementById("body").style.marginLeft = "13rem";
+  document.getElementById("mySidebar").style.width = "15%";
+  document.getElementById("body").style.width = "85%";
+  document.getElementById("body").style.marginLeft = "15%";
+  document.getElementById("nvarbar").style.width = "85%";
+  document.getElementById("nvarbar").style.marginLeft = "15%";
+  document.getElementById("footer-info").style.width = "85%";
+  document.getElementById("footer-info").style.marginLeft = "15%";
   x.style.display = "none";
 }
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("body").style.width = "100%";
   document.getElementById("body").style.marginLeft = "0";
+  document.getElementById("nvarbar").style.width = "100%";
+  document.getElementById("nvarbar").style.marginLeft = "0%";
+  document.getElementById("footer-info").style.width = "100%";
+  document.getElementById("footer-info").style.marginLeft = "0%";
   x.style.display = "block";
 }
+function changnavbar(pageName,name) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("recent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+    tablinks[i].style='border-bottom: none';
+  }
+  document.getElementById(pageName).style.display = "block";
+  document.getElementById(name).style='border-bottom: 2px solid';
+}
+document.getElementById("recent").click();
